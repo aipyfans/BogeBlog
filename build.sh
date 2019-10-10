314b0bef7140d749ec9eb7d8130af45bf2e71a2c
william <william@dream.com>
2019-10-10 09:08:28 +0800

    $1

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 #rm -rf docs && hexo clean && hexo deploy && mv public docs && git add -A && git commit -m "TO-DO" && git push origin master
 
-rm -rf docs && hexo clean && hexo generate && mv public docs && git add -A && git commit -m "$1" && git push origin master
+rm -rf docs && hexo clean && hexo generate && mv public docs && mv ads.txt docs && git add -A && git commit -m "$1" && git push origin master
